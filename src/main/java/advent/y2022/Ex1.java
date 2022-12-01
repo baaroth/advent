6e36b133dfad1c96846664474095c0c7aa2d7d1e
mathieu_lucas <mathieu.lucas.dev@gmail.com>
2022-12-01 07:15:28 +0100

    day 1

---
+++ src/main/java/advent/y2022/Ex1.java
@@ -0,0 +1,44 @@
+package advent.y2022;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.function.Consumer;
+
+public class Ex1 implements Consumer<String> {
+    private final int[] max = {-1, -1, -1};
+    private int current = -1;
+
+    @Override
+    public void accept(String cal) {
+        if ("".equals(cal)) {
+            current = -1;
+            return;
+        }
+
+        int v = Integer.parseInt(cal, 10);
+        if (current == -1) {
+            current = v;
+        } else {
+            current += v;
+        }
+        for (int i = 0; i < max.length;  ++i) {
+            if (current > max[i]) {
+                int remaining = max.length - i - 1;
+                if (remaining > 0) System.arraycopy(max, i, max, i + 1, remaining);
+                max[i] = current;
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        Ex1 consumer = new Ex1();
+        URI input = consumer.getClass().getResource("ex1.input.txt").toURI();
+        Files.readAllLines(Path.of(input)).forEach(consumer);
+        System.out.println(Arrays.stream(consumer.max).summaryStatistics());
+    }
+}
