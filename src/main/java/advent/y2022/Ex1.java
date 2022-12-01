1c4b41d8c187a5c65a2db8eb539bd906bf68d843
mathieu_lucas <mathieu.lucas.dev@gmail.com>
2022-12-01 07:36:19 +0100

    fix incomplete max spillover

--- src/main/java/advent/y2022/Ex1.java
+++ src/main/java/advent/y2022/Ex1.java
@@ -6,6 +6,7 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.function.Consumer;
 
 public class Ex1 implements Consumer<String> {
@@ -20,13 +21,17 @@ public class Ex1 implements Consumer<String> {
         }
 
         int v = Integer.parseInt(cal, 10);
-        if (current == -1) {
+        int last = current;
+        if (last == -1) {
             current = v;
         } else {
             current += v;
         }
         for (int i = 0; i < max.length;  ++i) {
-            if (current > max[i]) {
+            if (last == max[i]) {
+                max[i] = current;
+                return;
+            } else if (current > max[i]) {
                 int remaining = max.length - i - 1;
                 if (remaining > 0) System.arraycopy(max, i, max, i + 1, remaining);
                 max[i] = current;
@@ -34,11 +39,14 @@ public class Ex1 implements Consumer<String> {
             }
         }
     }
+    IntSummaryStatistics stats() {
+        return Arrays.stream(max).summaryStatistics();
+    }
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         Ex1 consumer = new Ex1();
         URI input = consumer.getClass().getResource("ex1.input.txt").toURI();
         Files.readAllLines(Path.of(input)).forEach(consumer);
-        System.out.println(Arrays.stream(consumer.max).summaryStatistics());
+        System.out.println(consumer.stats());
     }
 }
