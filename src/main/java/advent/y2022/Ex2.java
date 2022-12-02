23efad1e43eaad262ee23efb814812d41c0f7bfd
mathieu_lucas <mathieu.lucas.dev@gmail.com>
2022-12-02 23:26:35 +0100

    move dict to relevant enum
    also fix CRLF→LF

--- src/main/java/advent/y2022/Ex2.java
+++ src/main/java/advent/y2022/Ex2.java
@@ -9,36 +9,56 @@ import java.util.Map;
 import java.util.function.Consumer;
 
 public class Ex2 implements Consumer<String> {
-    private enum Shape {
-        ROCK(1),
-        PAPER(2),
-        SCISSORS(3);
 
-        final int score;
+    private int score = 0;
 
-        Shape(int score) {
-            this.score = score;
-        }
+    @Override
+    public void accept(String draw) {
+        score += compute2(draw);
     }
-    private static final Map<Character, Shape> OTHER_CODES = Map.of(
-            'A', Shape.ROCK,
-            'B', Shape.PAPER,
-            'C', Shape.SCISSORS
-    );
+
     private static final Map<Character, Shape> ME_CODES = Map.of(
             'X', Shape.ROCK,
             'Y', Shape.PAPER,
             'Z', Shape.SCISSORS
     );
+    private static int compute1(String draw) {
+        Shape other = Shape.BY_CODE.get(draw.charAt(0));
+        Shape me = ME_CODES.get(draw.charAt(2));
+        Outcome outcome = Outcome.evalFirst(me, other);
+        return me.score + outcome.score;
+    }
 
+    private int compute2(String draw) {
+        Shape other = Shape.BY_CODE.get(draw.charAt(0));
+        Outcome outcome = Outcome.BY_CODE.get(draw.charAt(2));
+        Shape me = switch (outcome) {
+            case DRAW -> other;
+            case WIN -> Outcome.LOOSING.get(other);
+            case LOSS -> Outcome.WINNING.get(other);
+        };
+        return me.score + outcome.score;
+    }
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        Ex2 consumer = new Ex2();
+        URI input = consumer.getClass().getResource("ex2.input.txt").toURI();
+        Files.readAllLines(Path.of(input)).forEach(consumer);
+        System.out.println(consumer.score);
+    }
 
     private enum Outcome {
         WIN(6),
         DRAW(3),
         LOSS(0);
 
-        final int score;
+        private static final Map<Character, Outcome> BY_CODE = Map.of(
+                'X', LOSS,
+                'Y', DRAW,
+                'Z', WIN
+        );
 
+        final int score;
         Outcome(int score) {
             this.score = score;
         }
@@ -53,46 +73,27 @@ public class Ex2 implements Consumer<String> {
                 Shape.SCISSORS, Shape.PAPER,
                 Shape.ROCK, Shape.SCISSORS
         );
-        static Outcome evalMe(Shape other, Shape me) {
-            if (other == me) return DRAW;
-            return WINNING.get(me) == other ? WIN : LOSS;
+        static Outcome evalFirst(Shape a, Shape b) {
+            if (a == b) return DRAW;
+            return WINNING.get(a) == b ? WIN : LOSS;
         }
     }
-    private static final Map<Character, Outcome> OUTCOME_CODES = Map.of(
-            'X', Outcome.LOSS,
-            'Y', Outcome.DRAW,
-            'Z', Outcome.WIN
-    );
-    private int score = 0;
 
-    @Override
-    public void accept(String draw) {
-        score += compute2(draw);
-    }
+    private enum Shape {
+        ROCK(1),
+        PAPER(2),
+        SCISSORS(3);
 
-    private int compute1(String draw) {
-        Shape other = OTHER_CODES.get(draw.charAt(0));
-        Shape me = ME_CODES.get(draw.charAt(2));
-        Outcome outcome = Outcome.evalMe(other, me);
-        return me.score + outcome.score;
-    }
+        static final Map<Character, Shape> BY_CODE = Map.of(
+                'A', ROCK,
+                'B', PAPER,
+                'C', SCISSORS
+        );
 
-    private int compute2(String draw) {
-        Shape other = OTHER_CODES.get(draw.charAt(0));
-        Outcome outcome = OUTCOME_CODES.get(draw.charAt(2));
-        Shape me = switch (outcome) {
-            case DRAW -> other;
-            case WIN -> Outcome.LOOSING.get(other);
-            case LOSS -> Outcome.WINNING.get(other);
-        };
-        return me.score + outcome.score;
-    }
-
-    public static void main(String[] args) throws IOException, URISyntaxException {
-        Ex2 consumer = new Ex2();
-        URI input = consumer.getClass().getResource("ex2.input.txt").toURI();
-        Files.readAllLines(Path.of(input)).forEach(consumer);
-        System.out.println(consumer.score);
+        final int score;
+        Shape(int score) {
+            this.score = score;
+        }
     }
 
 }
