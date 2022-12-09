5dcca41a97e511b34ca5d76d4649ca3cc496d615
mathieu_lucas <mathieu.lucas.dev@gmail.com>
2022-12-09 09:50:17 +0100

    day 9 part 2

--- src/main/java/advent/y2022/Ex9.java
+++ src/main/java/advent/y2022/Ex9.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -13,18 +14,22 @@ import java.util.regex.Pattern;
 public class Ex9 {
 
     private static final Pattern MOVE_FORMAT = Pattern.compile("^([UDRL]) (\\d+)$");
+    public static final int TRAILING_LEN = 9;
+    private static final int TAIL_IDX = TRAILING_LEN - 1;
+
     private Coor head = new Coor(0, 0);
-    private Coor tail = head;
-    private final Set<Coor> visited = new HashSet<>();
+    private final Coor[] knots = new Coor[TRAILING_LEN];
+    private final Set<Coor> tailVisited = new HashSet<>();
     private Ex9() {
-        visited.add(tail); // starting position is considered visited
+        Arrays.fill(knots, head);
+        tailVisited.add(knots[TAIL_IDX]); // starting position is considered visited
     }
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         URI input = Ex9.class.getResource("ex9.input.txt").toURI();
         Ex9 ex = new Ex9();
         Files.readAllLines(Path.of(input)).forEach(ex::moveHead);
-        System.out.println(ex.visited.size());
+        System.out.println(ex.tailVisited.size());
     }
 
     private void moveHead(String encoded) {
@@ -35,8 +40,8 @@ public class Ex9 {
         int steps = Integer.parseInt(m.group(2));
         while (steps > 0) {
             head = head.move(direction);
-            tail = head.pull(tail);
-            visited.add(tail);
+            head.pullAll(knots);
+            tailVisited.add(knots[TAIL_IDX]);
             --steps;
         }
     }
@@ -47,12 +52,14 @@ public class Ex9 {
 
     private record Coor(int x, int y) {
 
-        Coor pull(Coor tail) {
-            if (stable(tail)) return tail;
-            Coor after = tail;
-            if (x != tail.x) after = x > tail.x ? after.right() : after.left();
-            if (y != tail.y) after = y > tail.y ? after.up() : after.down();
-            return after;
+        public Coor pull(Coor tail) {
+            return stable(tail) ? tail : tail.follow(this);
+        }
+        public void pullAll(Coor[] tail) {
+            Coor last = this;
+            for (int i = 0; i < TRAILING_LEN && !last.stable(tail[i]); ++i) {
+                tail[i] = last = tail[i].follow(last);
+            }
         }
 
         public Coor move(Direction d) {
@@ -62,6 +69,13 @@ public class Ex9 {
                 case L -> left();
                 case R -> right();
             };
+        }
+
+        private Coor follow(Coor ref) {
+            Coor c = this;
+            if (x != ref.x) c = x < ref.x ? c.right() : c.left();
+            if (y != ref.y) c = y < ref.y ? c.up() : c.down();
+            return c;
         }
 
         private boolean stable(Coor tail) {
